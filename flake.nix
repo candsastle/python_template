3641964d2754626e48f632394f202547b8d975ee
candsastle <tsaharya@pm.me>
2026-05-18 23:42:35 -0400

    added everything

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Generic Template Flake For Python Projects";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+  outputs = {self, ...} @ inputs: let
+    pkgs = inputs.nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells = {
+      "x86_64-linux".default = pkgs.mkShell {
+        buildInputs = with pkgs; [uv];
+
+        shellHook =
+          /*
+          bash
+          */
+          ''
+            if [ ! -f ./.init ]; then
+              uv sync && echo "This file indicates that you have already run uv sync in this directory" > .init
+            fi
+          '';
+      };
+    };
+  };
+}
